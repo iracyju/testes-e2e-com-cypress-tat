e20048e51caef723464239edcbf81813dafefc2e
Iracy Juliana <iracyjuliana.ribeiro@gmail.com>
2025-07-18 19:13:24 -0300

    Break lint and tests on purpose

--- cypress/e2e/1-getting-started/authenticated.cy.js
+++ cypress/e2e/1-getting-started/authenticated.cy.js
@@ -37,12 +37,8 @@ describe('Scenarios where authentication is a pre-condition', () => {
     
     () => {
     cy.visit('/')
-    cy.wait('@getNotes')
-    if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
-    cy.get('.navbar-toggle.collapsed')
-    .should('be.visible')
-    .click()
-}
+    cy.wait(2000)
+   
 
 
     cy.contains('.nav a', 'Logout').click()
